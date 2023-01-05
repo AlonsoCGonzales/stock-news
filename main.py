37599b6a57ef96abe255a5132653e262725b4322
AlonsoCGonzales <alonzotoricaceres@gmail.com>
2023-01-05 18:16:35 -0500

    stock

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
+from twilio.rest import Client
+
+number = "976316061"
 
 STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+COMPANY_NAME = "Tesla inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "OTYDDGO0LXKTMW0M"
 NEWS_API_KEY = "bf5dbd10e0104f2f8cd45b652ec7ec4e"
+TWILIO_SID = "AC0b20d352650300837c483d8b844fb314"
+TWILIO_AUTH_TOKEN = "01beb97ab9ea52dd2ee9c58447095ba2"
 
 url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo"
 
@@ -28,13 +33,17 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
-
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = 0
+if difference > 1:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+    
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
-if diff_percent > 1:
+if abs(diff_percent) > 0:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -50,10 +59,17 @@ if diff_percent > 1:
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 #to send a separate message with each article's title and description to your phone number.
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio.
-
+#Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}, \nBrief: {article['description']}" for article in three_articles]
+#Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    
+    for article in formatted_articles:
+        message = client.messages.create(
+            body = article,
+            from_= "+13464894632",
+            to = "+51968509004"
+        )
 #Optional TODO: Format the message like this:
 """
 TSLA: 🔺2%
